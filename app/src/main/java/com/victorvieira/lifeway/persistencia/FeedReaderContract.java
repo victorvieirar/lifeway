e85eb03b1fba9fd1b3f8bbbf971a2e89724033f9
Rayanne Oliveira <rayanne992010@hotmail.com>
2016-08-12 10:50:56 -0300

    Version 0.4.4 Working with basic info, historic and details on historic Database in development
    # Version: 0.4.4
    # Class FeedReaderContract added
    # New UI Theme (in development)

---
+++ app/src/main/java/com/victorvieira/lifeway/persistencia/FeedReaderContract.java
@@ -0,0 +1,38 @@
+package com.victorvieira.lifeway.persistencia;
+
+import android.provider.BaseColumns;
+
+public final class FeedReaderContract {
+
+    public FeedReaderContract() {}
+
+    public static abstract class FeedEntry implements BaseColumns {
+        /** Usuário */
+        public static final String USER_TABLE_NAME = "usuario";
+        public static final String USER_COLUMN_ID = "_id";
+        public static final String USER_COLUMN_NOME="nome";
+        public static final String USER_COLUMN_PESO="peso";
+        public static final String USER_COLUMN_ALTURA="altura";
+        public static final String USER_COLUMN_IMC="imc";
+        public static final String USER_COLUMN_META="meta";
+
+        /** Consumo */
+        public static final String CONSUMO_TABLE_NAME = "consumo";
+        public static final String CONSUMO_COLUMN_ID = "_id";
+        public static final String CONSUMO_COLUMN_ID_REF = "_id_ref";
+
+        /** Refeição*/
+        public static final String REFEICAO_TABLE_NAME = "refeicao";
+        public static final String REFEICAO_COLUMN_ID = "_id";
+        public static final String REFEICAO_COLUMN_ID_ALI = "_id_ali";
+
+        /** Alimento*/
+        public static final String ALIMENTO_TABLE_NAME = "alimento";
+        public static final String ALIMENTO_COLUMN_ID = "_id";
+        public static final String ALIMENTO_COLUMN_NOME = "nome";
+        public static final String ALIMENTO_COLUMN_TIPO = "tipo";
+        public static final String ALIMENTO_COLUMN_INDICACAO = "indicacao";
+        public static final String ALIMENTO_COLUMN_VALOR_CALORICO = "valor_calorico";
+
+    }
+}
